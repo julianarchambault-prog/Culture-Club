aef4164b9d352f301e24bfdb58748efa827e801b
julianarchambault-prog <julianarchambault@gmail.com>
2025-11-04 00:52:39 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import CultureClubApp from "./App.jsx"; // make sure the path matches
-import "./index.css";
+import App from "./App.jsx";  // note the capital "A"
+import "../index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CultureClubApp />
+    <App />
   </React.StrictMode>
 );
